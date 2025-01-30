70ffb2f5168ffa8ed837f93483a046961fb60f4a
Priyanku Gogoi <workwithpriyanku@gmail.com>
2025-01-30 10:59:03 +0530

    mocking the servers

---
+++ load-balancer/mock/server02/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	port := "8082"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Response from server %s", port)
+	})
+	http.ListenAndServe(":"+port, nil)
+}
