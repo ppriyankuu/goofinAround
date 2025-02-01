9753303f5c472f7c85b1a1699e485b3fe1bf5ce9
Priyanku Gogoi <workwithpriyanku@gmail.com>
2025-02-01 11:42:56 +0530

    removed unnecessary code

--- task-api/models/task.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Task struct {
-	ID          int    `db:"id" json:"id"`
-	Title       string `db:"title" json:"title"`
-	Description string `db:"description" json:"description"`
-	Status      string `db:"status" json:"status"`
-	CreatedAt   string `db:"created_at" json:"created_at"`
-}
